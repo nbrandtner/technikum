bf145f6383ff098ccabd107db573be2785708d28
nbrandtner-tgm <nbrandtner@student.tgm.ac.at>
2023-09-19 08:00:34 +0200

    add anrechnungen, change index.php

---
+++ Anrechnungen.md
@@ -0,0 +1,15 @@
+# Anrechnungen:
+
+TGM Wien - Informationstechnologie:
+Netzwerktechnik: 1&2. Jgg: 2h/Woche > ~160h; 3. Jgg: 3h/Woche > ~120h;
+Systemtechnik: 1-3. Jgg 4h/Woche > ~480h;
+
+
+
+In Netzwerktechnik haben wir in den ersten drei Jahrgängen einige relevante Themen bearbeitet:
+- Basisinformationen über Netzwerke und Protokolle (OSI Schichten Modell, Netzwerk-Topologien, Network-Arten [LAN,WAN,GAN],Funktionsweise des Internets [DNS, Protokolle wie TCP,IP,UDP]);
+- Simulierung von Netzwerken, deren Aufbau sowie Konfigurierung in Cisco Packet Tracer
+
+In Systemtechnik wurden uns ebenfalls in den ersten drei Jahren Infrastruktur Inhalte gelehrt:
+
+- 
--- WEB/01-Semesterprojekt/index.php
+++ WEB/01-Semesterprojekt/index.php
@@ -1,3 +1,15 @@
+<?php
+session_start(); // Sitzung starten, um den Benutzerstatus zu speichern
+
+// Überprüfen Sie, ob der Benutzer angemeldet ist und welche Rolle er hat
+$role = "anonymous"; // Standardrolle
+
+if (isset($_SESSION['user_role'])) {
+    $role = $_SESSION['user_role'];
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,16 +23,20 @@
         <h1>Hotel Webseite</h1>
         <!-- Hier kannst du die Navigationsleiste für verschiedene Benutzerrollen erstellen -->
         <nav>
+            <a href="index.php">Startseite</a>
+            <a href="help.php">Hilfe/FAQs</a>
+            <a href="impressum.php">Impressum</a>
+            <a href="news.php">News-Beiträge</a>
             <?php
-            // Überprüfe die Benutzerrolle und zeige entsprechende Links an
-            if (/* Benutzer ist eingeloggt und Administrator */) {
-                echo '<a href="admin/news.php">News verwalten</a>';
-            } elseif (/* Benutzer ist eingeloggt */) {
-                echo '<a href="profile.php">Profil</a>';
-                echo '<a href="reservations.php">Reservierungen</a>';
-            } else {
+            if ($role === "anonymous") {
                 echo '<a href="register.php">Registrieren</a>';
-                echo '<a href="login.php">Anmelden</a>';
+            } elseif ($role === "user") {
+                echo '<a href="reservation.php">Zimmerreservierung</a>';
+                echo '<a href="profile.php">Profil</a>';
+            } elseif ($role === "admin") {
+                echo '<a href="admin/news_management.php">News verwalten</a>';
+                echo '<a href="admin/reservation_management.php">Reservierungen verwalten</a>';
+                echo '<a href="admin/user_management.php">Userverwaltung</a>';
             }
             ?>
         </nav>
