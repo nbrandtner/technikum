efdb771b27a013e05229f0b051dc57aca2228488
nbrandtner-tgm <nbrandtner@student.tgm.ac.at>
2023-09-19 08:08:53 +0200

    add help.php and impressum.php

---
+++ WEB/01-Semesterprojekt/impressum.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Impressum</title>
+    <!-- Füge hier deine CSS-Dateien hinzu, um das Styling anzupassen -->
+</head>
+<body>
+    <header>
+        <h1>Impressum</h1>
+        <nav>
+            <a href="index.php">Startseite</a>
+            <a href="help.php">Hilfe/FAQs</a>
+            <a href="impressum.php">Impressum</a>
+            <!-- Weitere Navigationslinks für angemeldete Benutzer -->
+        </nav>
+    </header>
+
+    <main>
+        <h2>Impressum</h2>
+        <p>Dieses Impressum gilt für die Website der Hotelverwaltung:</p>
+
+        <p><strong>Firma:</strong> Hotel XYZ</p>
+        <p><strong>Adresse:</strong> Musterstraße 123, 12345 Musterstadt</p>
+        <p><strong>Telefon:</strong> +49 123 456789</p>
+        <p><strong>E-Mail:</strong> info@hotelxyz.com</p>
+
+        <h3>Projektteam</h3>
+        <p>Beteiligte Studierende am Projekt:</p>
+        <ul>
+            <li>Name 1</li>
+            <li>Name 2</li>
+            <li>Name 3</li>
+            <!-- Füge weitere Namen hinzu -->
+        </ul>
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Hotel Webseite</p>
+    </footer>
+</body>
+</html>
