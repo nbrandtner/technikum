100892b22d56867a796bed37c9180788c42bfe65
Mikado <elaine.fink19@gmail.com>
2024-01-11 16:53:34 +0100

    new button names

--- WEB/01-Semesterprojekt/UE01/header.php
+++ WEB/01-Semesterprojekt/UE01/header.php
@@ -6,13 +6,16 @@
                 <button type="button" onclick="window.location.href='login.php'" href="login.php"class="glow-on-hover upper-corner">Login</button>
         <?php endif; ?>
 </nav>
-        <header>
-                <a href="index.php"><img src="img/logo-transparent.png" width="330px"></a>
-                <nav>
-                <button onclick="window.location.href='reservation.php'" type="button" href="reservation.php" class="glow-on-hover" alt="Reservation">Reservation</button>              
+ <header>
+        <a href="index.php"><img src="img/logo-transparent.png" width="330px"></a>
+        <nav>
+                <button onclick="window.location.href='reservation.php'" type="button" href="reservation.php" class="glow-on-hover" alt="Reservation">Booking</button>              
                 <button onclick="window.location.href='index.php'" type="button" href="index.php" class="glow-on-hover" alt="Homepage">Homepage</button>
                 <button onclick="window.location.href='help.php'" type="button" href="help.php" class="glow-on-hover" alt="Help/FAQ">Help/FAQs</button>
                 <button onclick="window.location.href='impressum.php'" type="button" href="impressum.php" class="glow-on-hover" alt="Impressum">Impressum</button>
                 <!-- Weitere Navigationslinks für angemeldete Benutzer -->
-                </nav>
-                </header>    
+                <?php if(isset($_SESSION['loggedin'])): ?>
+                        <button onclick="window.location.href='reservationlist.php'" type="button" href="reservation.php" class="glow-on-hover" alt="Reservation">Reservations</button>              
+                <?php endif; ?>
+        </nav>
+</header>    
