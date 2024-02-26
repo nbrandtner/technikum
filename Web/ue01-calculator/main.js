cbffceff0e8125b705295b3b2f9647791aff3bd3
nbrandtner <nbrandtner@student.tgm.ac.at>
2024-02-26 01:09:39 +0100

    finish calculator

--- Web/ue01-calculator/main.js
+++ Web/ue01-calculator/main.js
@@ -2,7 +2,14 @@ function calc(operation) {
 	var zahl1 = document.getElementById("zahl1").value;
 	var zahl2 = document.getElementById("zahl2").value;
 	var ergebnis = 0;
-	// idk ob ich das übergebene rechenzeichen auch direkt verwenden könnte also hab ichs mit switch-case gemacht
+	// delete whitespaces and leading zeros
+	// https://stackoverflow.com/questions/6676488/remove-leading-zeros-from-a-number-in-javascript (solution to remove leading zeros)
+	zahl1 = zahl1.trim();
+	zahl1 = zahl1.replace(/^0+/, "");
+	zahl2 = zahl2.trim();
+	zahl2 = zahl2.replace(/^0+/, "");
+
+	// idk if i couldve used the operation directly (zahl1 + operation + zahl2) or smth like that so i used a switch-case instead
 	switch (operation) {
 		case "+":
 			ergebnis = parseFloat(zahl1) + parseFloat(zahl2);
