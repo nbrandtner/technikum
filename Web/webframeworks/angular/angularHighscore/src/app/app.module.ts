a5b46a4f480c976cda6a7fe2fa2901c631649fa8
nbrandtner <nbrandtner@gmx.at>
2024-05-16 10:10:02 +0200

    signup added

--- Web/webframeworks/angular/angularHighscore/src/app/app.module.ts
+++ Web/webframeworks/angular/angularHighscore/src/app/app.module.ts
@@ -4,16 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Web/webframeworks/angular/angularHighscore/src/app/login/login.component.ts
+++ Web/webframeworks/angular/angularHighscore/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  email = '';
-  password = '';
-  confirmPassword = '';
+  emailLogIn = new FormControl('', [Validators.required, Validators.email]);
+  passwordLogIn = new FormControl('', [Validators.required]);
+  hide = true;
+
+  getErrorMessage() {
+    if (this.emailLogIn.hasError('required') || this.passwordLogIn.hasError('required')) {
+      return 'You have to enter an Email and a Password.';
+    }
+    return this.emailLogIn.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  login() {
+    if (this.emailLogIn.value === 'test@test.at' && this.passwordLogIn.value === '12345678') {
+      console.log('Login successful.');
+    } else {
+      console.log('Login failed.');
+    }
+  }
 }
